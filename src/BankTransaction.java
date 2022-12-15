bf948493c0e863be3f64aad660859e3e1e01fcc5
Enrique <enriquedlc2d@gmail.com>
2022-12-15 10:42:54 +0000

    six2

--- src/BankTransaction.java
+++ src/BankTransaction.java
@@ -6,12 +6,12 @@ public class BankTransaction {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount(100);
         /**
-                * The account owner has a name, a quantity of money to deposit or extract, the type of action and the bank account.
-                * The account is shared between the users.
+         * The account owner has a name, a quantity of money to deposit or extract, the type of action and the bank account.
+         * The account is shared between the users.
          *
          * First the account balance was showed straight away, but now it is showed after the action is done.
-                * Adding the synchronized keyword to the methods in the BankAccount class, the balance is showed correctly.
-                */
+         * Adding the synchronized keyword to the methods in the BankAccount class, the balance is showed correctly.
+         */
         AccountOwnerThread juan = new AccountOwnerThread("John", 20, ActionEnum.EXTRACT, bankAccount);
         AccountOwnerThread laura = new AccountOwnerThread("Maria", 40, ActionEnum.EXTRACT, bankAccount);
         AccountOwnerThread antonio = new AccountOwnerThread("Peter", 35, ActionEnum.DEPOSIT, bankAccount);
